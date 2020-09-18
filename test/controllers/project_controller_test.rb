0a57493793a0ee90105cf19786c8a3e9cd9b79c5
Andrew Nicholson <andrew.nicholson4491@gmail.com>
2020-09-18 19:46:41 -0400

    fix tests

--- test/controllers/project_controller_test.rb
+++ test/controllers/project_controller_test.rb
@@ -24,6 +24,22 @@ class ProjectControllerTest < ActiveSupport::TestCase
     get project_index_url(phase_actual_start_date: '07/07/2016')
     assert_response :success
 
-    assert_equal(['I.S. 254 - BRONX test search string', 'I.S. 254 - BRONX'], JSON.parse(response.body).map {|p| p['school_name'] })
+    assert_equal(['I.S. 254 - BRONX test search string', 'I.S. 254 - BRONX', "DEWITT CLINTON HS - X", "DEWITT CLINTON HS - X", "P.S. 14 - BRONX", "P.S. 14 - BRONX", "P.S. 130 - MANHATTAN", "MULTICULTURAL HS - BROOKLYN"], JSON.parse(response.body).map {|p| p['school_name'] })
+  end
+
+  test "should update phase_actual_start_date and total_phase_actual_spending" do
+    project = Project.first
+    put project_url(
+      project,
+      {
+        phase_actual_start_date: project.phase_actual_start_date + 1.day,
+        total_phase_actual_spending: project.total_phase_actual_spending + 100000000
+      }
+    )
+    assert_response :success
+
+    response_project = JSON.parse(response.body)['project']
+    assert_equal((project.phase_actual_start_date + 1.day).to_s, response_project['phase_actual_start_date'])
+    assert_equal(project.total_phase_actual_spending + 100000000, response_project['total_phase_actual_spending'])
   end
 end
