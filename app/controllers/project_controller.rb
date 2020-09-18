7d09f6876d6763ff34b3c49efc15373c0c26cd11
Andrew Nicholson <andrew.nicholson4491@gmail.com>
2020-09-18 19:23:20 -0400

    add filterable attributes

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -10,7 +10,12 @@ class ProjectController < ApplicationController
 
   private def filter_params
     params.permit(
-      :phase_actual_start_date
+      :phase_actual_start_date,
+      :phase_planned_end_date,
+      :phase_planned_end_date,
+      :budget,
+      :final_estimate_of_actual_costs,
+      :total_phase_actual_spending
     ).to_h
   end
 end
